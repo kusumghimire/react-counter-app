369d6b6c6918a0f77b4f141b1e99ce0c68c72230
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-05 17:47:38 +0545

    update: formatCount method created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,11 +1,26 @@
 import React, {Component} from 'react'
 
 class Counter extends Component{
-    state ={}
+    state = {
+        count :0,
+    } ;
 
     render (){
-     return <h1>Namaste</h1>
+     return (
+         <div>
+         <span>{this.formatCount()}</span>
+         <button>Increment</button>
+          </div>
+         )    
+         
+         
     }
+
+    formatCount(){
+        const {count} = this.state;
+        return count === 0 ? 'Zero' : count;
+    }
+
 }
 
 export default Counter;
