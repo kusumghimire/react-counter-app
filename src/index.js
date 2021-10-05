73b49fc454f1e23f4e1754d0e493f0cebc5b6026
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-05 17:34:15 +0545

    Update: counter.jsx is created

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/css/bootstrap.css';
+import Counter from './components/counter';
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
