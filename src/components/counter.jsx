4e49aae34d3654e39d5013f5df0e96db20461446
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-07 23:01:41 +0545

    arrow function is added to event handlers

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -16,13 +16,13 @@ class Counter extends Component {
   // Handle input method is used to bind the method
   // Note: Functions in js are objects so they have properties and methods so here bind is used.
   // Instead of adding reassign function we can add arrow function
-
+   
   render() {
     return (
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={this.handleIncrement}
+          onClick={() => this.handleIncrement ({product})}
           className="btn btn-secondary btn-sm"
         >
           Increment
@@ -30,7 +30,7 @@ class Counter extends Component {
       </div>
     );
   }
-
+// Whenever need to pass arrow functions event handlers simply pass the body of the function and eventhandlers
   getBadgeClasses() {
     let classes = "btn m-2 btn-";
     classes += this.state.count === 0 ? "warning" : "primary";
@@ -44,3 +44,6 @@ class Counter extends Component {
 }
 
 export default Counter;
+
+
+// Note: react create new virtual DOM
