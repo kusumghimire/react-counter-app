1e32305ec7c96f22b1332a6058d1ce53eafb7084
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-07 17:35:49 +0545

    renderTags is added

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -11,32 +11,24 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
+  renderTags(){
+    if(this.state.tags.length === 0 ) return <p>There are no tags !</p>;
+   return <ul>
+    {this.state.tags.map(tag => 
+     <li key={tag}>{tag}</li>)
+    }
+  </ul>
+  }
+
   render() {
 
     return (
       <div>
-        <span className={this.getBadgeClasses()}>
-          {this.formatCount()}
-        </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => <li key={tag}>{tag}</li>)}
-
-        </ul>
+        {this.renderTags()}
       </div>
     );
   }
 
-  getBadgeClasses(){
-    let classes = "btn m-2 btn-";
-    classes += this.state.count === 0 ? 'warning' : 'primary';
-    return classes;
-  }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
-  }
 }
 
 export default Counter;
