e7d5fd9336f2e5af1398113a5efab7ab97ed3b3a
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-07 23:17:39 +0545

    product is passed as an argument

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -10,7 +10,8 @@ class Counter extends Component {
     fontWeight: "bold",
   };
 
-  handleIncrement = () => {
+  handleIncrement = product => {
+    console.log(product)
     this.setState({count: this.state.count +1});
   }
   // Handle input method is used to bind the method
@@ -22,7 +23,7 @@ class Counter extends Component {
       <div>
         <span className={this.getBadgeClasses()}>{this.formatCount()}</span>
         <button
-          onClick={() => this.handleIncrement ({product})}
+          onClick={() => this.handleIncrement (product)}
           className="btn btn-secondary btn-sm"
         >
           Increment
