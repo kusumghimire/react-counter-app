0d77b3067a20cfca6ed9d4aa13fd727f5c733774
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-07 17:30:14 +0545

    lists are rendering

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -3,6 +3,7 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
+    tags: ['tag1','tag2','tag3']
   };
 
   styles = {
@@ -18,6 +19,10 @@ class Counter extends Component {
           {this.formatCount()}
         </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => <li>{tag}</li>)}
+
+        </ul>
       </div>
     );
   }
