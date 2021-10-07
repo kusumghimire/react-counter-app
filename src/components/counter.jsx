969d200e91a82ccf7fcd5bfb04cb02171739f458
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-07 17:11:35 +0545

    this.style is added to badge

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,16 +2,20 @@ import React, {Component} from 'react'
 
 class Counter extends Component{
     state = {
-        count :1,
-        imageUrl:'https://picsum.photos/200'
+        count :0,
     } ;
+
+    styles={
+        fontSize:"10px",
+        fontWeight: "bold",
+    }
 
     render (){
      return (
          <div>
-         <img src={this.state.imageUrl} alt="" />
-         <span>{this.formatCount()}</span>
-         <button>Increment</button>
+   
+         <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
+         <button className="btn btn-secondary btn-sm" >Increment</button>
           </div>
          )                      
     }
