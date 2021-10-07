1e7effc503e4ea12cc3050cdaa80f1dafac67b56
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-07 17:20:34 +0545

    dynamic class added to counter function

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,31 +1,33 @@
-import React, {Component} from 'react'
+import React, { Component } from "react";
 
-class Counter extends Component{
-    state = {
-        count :0,
-    } ;
+class Counter extends Component {
+  state = {
+    count: 0,
+  };
 
-    styles={
-        fontSize:"10px",
-        fontWeight: "bold",
-    }
+  styles = {
+    fontSize: "10px",
+    fontWeight: "bold",
+  };
 
-    render (){
-     return (
-         <div>
-   
-         <span style={this.styles} className="badge badge-primary m-2">{this.formatCount()}</span>
-         <button className="btn btn-secondary btn-sm" >Increment</button>
-          </div>
-         )                      
-    }
+  render() {
+   let classes = "btn m-2 btn-";
+   classes += this.state.count === 0 ? 'warning' : 'primary';
 
-    formatCount(){
-        const {count} = this.state;
-        return count === 0 ? 'Zero' : count;
-    }
+    return (
+      <div>
+        <span className={classes}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+      </div>
+    );
+  }
 
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
+  }
 }
 
 export default Counter;
-
