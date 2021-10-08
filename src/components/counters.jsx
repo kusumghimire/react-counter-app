4f913f9146ce313838ee57704875bcd3a7774c96
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-08 15:47:19 +0545

    handleDelete is made functional

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -4,14 +4,15 @@ class Counters extends Component{
     state= {
         counters:[
             {id:1, value:4},   
-            {id:1, value:0},   
-            {id:1, value:0},   
-            {id:1, value:0},   
+            {id:2, value:0},   
+            {id:3, value:0},   
+            {id:4, value:0},   
         ]
     };
 
-    handleDelete = () =>{
-       console.log("Event Handler Called");
+    handleDelete = (counterId) =>{
+       const counters = this.state.counters.filter(c => c.id !== counterId);
+       this.setState({counters})
     };
     render(){
         return(
