cfee81c163be6e85f813cdc8f0e17bfaa77c7a42
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-08 14:40:59 +0545

    mapping added to counter

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,10 +1,18 @@
 import React, { Component } from 'react'
 import Counter from './counter';
 class Counters extends Component{
-    state= {}
+    state= {
+        counters:[
+            {id:1, value:0},   
+            {id:1, value:0},   
+            {id:1, value:0},   
+            {id:1, value:0},   
+        ]
+    }
     render(){
         return(<div>
-
+          {this.state.counters.map(counter => <Counter key={counter.id} />)
+          }
         </div>);
     }
 }
