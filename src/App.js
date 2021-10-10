cf4cd1d04237b26b6108e075caeb347151b1aa25
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-10 22:31:40 +0545

    events are added into counters in app.js

--- src/App.js
+++ src/App.js
@@ -4,15 +4,50 @@ import "./App.css";
 import Navbar from "./components/navbar";
 import Counters from "./components/counters";
 
-function App() {
-  return (
-    <>
-      <Navbar />
-      <main className="container">
-        <Counters />
-      </main>
-    </>
-  );
+class App extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 0 },
+      { id: 4, value: 0 },
+    ],
+  };
+
+  handleIncrement = (counter) => {
+    const counters = [...this.state.counters];
+    const index = counters.indexOf(counter);
+    counters[index] = { ...counter };
+    counters[index].value++;
+    this.setState({ counters });
+  };
+
+  handleReset = () => {
+    const counters = this.state.counters.map((c) => {
+      c.value = 0;
+      return c;
+    });
+    this.setState({ counters });
+  };
+  handleDelete = (counterId) => {
+    const counters = this.state.counters.filter((c) => c.id !== counterId);
+    this.setState({ counters });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar />
+        <main className="container">
+          <Counters 
+          onReset ={this.handleReset} 
+          onIncrement = {this.handleIncrement}
+          onDelete= {this.handleDelete}
+          />
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
