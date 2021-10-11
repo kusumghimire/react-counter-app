154d7bb468a30492efbcd38606ee6fd6c70a8085
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-11 22:36:46 +0545

    object destructuring is used

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,16 +2,17 @@ import React, { Component } from 'react'
 import Counter from './counter';
 class Counters extends Component{
     render(){
+        const {onReset, counters, onDelete, onIncrement} = this.props;
         return(
         <div>
-            <button className="btn btn-primary btn-sm m-2" onClick={this.props.onReset}>
+            <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
                 Reset
             </button>
-          {this.props.counters.map(counter => (
+          {counters.map(counter => (
           <Counter 
           key={counter.id} 
-          onDelete={this.props.onDelete}
-          onIncrement = {this.props.onIncrement}
+          onDelete={onDelete}
+          onIncrement = {onIncrement}
           counter = {counter} 
           />  //here value ,id are the parts of the props that can be access to another component
           ))}
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -2,15 +2,12 @@ import React, { Component } from "react";
 
 // Stateless Functional Component 
 
-const Navbar = (props) =>{
+const Navbar = ({totalCounters}) =>{
   return (
-    <React.Fragment>
       <nav className="navbar navbar-light bg-light">
         <span className="navbar-brand mb-0 h1">Navbar</span>
-        <span className="btn btn-secondary btn-sm">{props.totalCounters}</span>
-      
+        <span className="btn btn-secondary btn-sm">{totalCounters}</span>
       </nav>
-    </React.Fragment>
   );
 }
 
