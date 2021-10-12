66a9dc7621a2653116a2be63567730641548d8f8
Kusum Ghimire <kghimire@himalayantrchies.com>
2021-10-12 23:04:55 +0545

    onDecrement function created

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -32,7 +32,13 @@ class Counter extends Component {
           onClick={() => this.props.onIncrement(this.props.counter)}
           className="btn btn-secondary btn-sm"
         >
-          Increment
+          +
+        </button>
+        <button
+          onClick={() => this.props.onDecrement(this.props.counter)}
+          className="btn btn-secondary btn-sm"
+        >
+          -
         </button>
         <button onClick={()=> this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
       </div>
--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import Counter from './counter';
 class Counters extends Component{
     render(){
-        const {onReset, counters, onDelete, onIncrement} = this.props;
+        const {onReset, counters, onDelete, onIncrement, onDecrement} = this.props;
         return(
         <div>
             <button className="btn btn-primary btn-sm m-2" onClick={onReset}>
@@ -13,6 +13,7 @@ class Counters extends Component{
             key={counter.id} 
             onDelete={onDelete}
             onIncrement = {onIncrement}
+            onDecrement = {onDecrement}
             counter = {counter} 
             />  //here value ,id are the parts of the props that can be access to another component
             ))}
